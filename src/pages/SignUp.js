f7d585519d5b9cc74bb8078cb5186f9a12a99f79
qxan <mehmet.enfa@gmail.com>
2022-12-25 21:43:48 +0300

    joni medium

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,25 +4,20 @@ export default function SignIn() {
   return (
     <div className="h-screen w-screen border flex items-center justify-center">
       <div className="flex flex-col items-center text-center h-[687px] w-[695px] shadow-2xl">
-        <h1 className="text-3xl mt-[4rem]">Welcome back.</h1>
+        <h1 className="text-3xl mt-[4rem]">Join Medium.</h1>
         <div className="mt-[10rem]">
           <p>sign inputs</p>
           <p className="text-lg">
-            No account?
+          Already have an account?
             <button className="ml-1 text-createone">
-              <b>Create one</b>
+              <b>Sign in</b>
             </button>
           </p>
-          <p className="mt-[5rem]">
-            Forgot email or trouble signing in?{" "}
-            <a href="#" className="underline">
-              Get help.
-            </a>
-          </p>
+          
         </div>
         <div>
           <p className="mt-[3rem]">
-            Click “Sign In” to agree to Mediums{" "}
+            Click “Sign Up” to agree to Mediums{" "}
             <a href="#" className="underline">
               Terms of Services
             </a>{" "}
